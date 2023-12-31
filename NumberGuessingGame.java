95a74cff21054660d17475678dd976ce5aa72751
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-12-31 23:52:15 +0530

    NumberGuessingGame done

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class NumberGame{
+public class NumberGuessingGame{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
